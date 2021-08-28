ac82f4d7691675ea75cd6058318a7bd4d99a4c1f
RafaelAnderson <rp_9966@hotmail.com>
2021-08-28 03:14:14 -0500

    Movimiento pelota y jugador

--- Juego 1 - Break Out/main.py
+++ Juego 1 - Break Out/main.py
@@ -3,6 +3,7 @@ import sys
 
 ANCHO = 640
 ALTO = 480
+color_negro = (0, 0, 0) # Color RGB
 
 class Pelota(pygame.sprite.Sprite):
     def __init__(self):
@@ -14,24 +15,63 @@ class Pelota(pygame.sprite.Sprite):
         # Posicion inicial
         self.rect.centerx = ANCHO / 2
         self.rect.centery = ALTO / 2
+        #Velocidad
+        self.speed = [3, 3]
+    def update(self):
+        # Evitar que salga por abajo y arriba
+        if self.rect.bottom >= ALTO or self.rect.top <= 0:
+            self.speed[1] = -self.speed[1]
+        # Evitar que salga por la derecha e izquierda
+        elif self.rect.right >= ANCHO or self.rect.left <= 0:
+            self.speed[0] = -self.speed[0]
+        self.rect.move_ip(self.speed)
+
+# Creando Barra
+class Jugador(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load("images/barra.png")
+        self.rect = self.image.get_rect()
+        self.rect.midbottom = (ANCHO / 2, ALTO - 20)
+        self.speed = [3, 0]
+    def update(self, evento):
+        # Flecha izquierda
+        if evento.key == pygame.K_LEFT and self.rect.left > 0:
+            self.speed = [-5, 0]
+        elif evento.key == pygame.K_RIGHT and self.rect.right < ANCHO:
+            self.speed = [5, 0]
+        else: self.speed = [0, 0]
+        self.rect.move_ip(self.speed)
 
 # Inicializando pantalla
 pantalla = pygame.display.set_mode((ANCHO, ALTO))
-
 #Configurando título
 pygame.display.set_caption('Break Out')
-
+# Creando el reloj
+reloj = pygame.time.Clock()
 # Objeto pelota
 pelota = Pelota()
+jugador = Jugador()
+# Repetición de evento de tecla presionada
+pygame.key.set_repeat(30)
 
+################### JUEGO ####################
 while True:
+    # Estableciendo frames
+    reloj.tick(75)
     #Revisando eventos
     for evento in pygame.event.get():
         if evento.type == pygame.QUIT:
             sys.exit()
+        elif evento.type == pygame.KEYDOWN:
+            jugador.update(evento)
 
-    # Dibujar pelota
+    #Actualizar posicion de la pelota
+    pelota.update()
+    #Rellenar fondo
+    pantalla.fill(color_negro)
+    # Dibujar pelota (blit dibuja una superficie sobre otra)
     pantalla.blit(pelota.image, pelota.rect)
-
+    pantalla.blit(jugador.image, jugador.rect)
     # Actualiza elementos de la pantalla
     pygame.display.flip()
